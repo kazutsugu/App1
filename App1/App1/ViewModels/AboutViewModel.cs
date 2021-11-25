36da7dc45c035b2f5ef39c7c4892036df60743dd
Fukumuro <kazutsugu.fukumuro@itu.int>
2021-11-25 16:50:57 +0100

    initial commit

---
+++ App1/App1/ViewModels/AboutViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            //Title = "About";
+            var lib = new ClassLibrary1.Class1();
+            Title = lib.HelloWorld();
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://xamarin.com"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public string HelloWorld()
+        {
+            return "Hello World From Shared Library!!";
+        }
+    }
+}
